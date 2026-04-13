e158a69c7c03c2b13c676d049fe15535364c0aa5
tuan2090 <tuan72972@gmail.com>
2026-04-13 20:08:24 +0700

    Modifer src/func-template.cpp file

--- src/func-template.cpp
+++ src/func-template.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-template <typename T>
-T addnum(T a, T b) {
+template <typename MATH_T>
+MATH_T addnum(MATH_T a, MATH_T b) {
   return a + b;
 }
 
 int main() {
-  std::cout << addnum(6, 3) << std::endl;
-  std::cout << addnum(6.3, 3.1) << std::endl;
+  std::cout << addnum<int>(6, 3) << std::endl;
+  std::cout << addnum<float>(6.3, 3.1) << std::endl;
 
   return 0;
 }
